f7b2f8f92be4952cb2aab9b24fb0e764e07803c1
Jesse Tran <jessetran03@gmail.com>
2021-11-13 02:48:36 -0600

    Added Log entry mutation

--- dist/graphql/mutations/index.js
+++ dist/graphql/mutations/index.js
@@ -3,6 +3,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+var addLogEntry_1 = __importDefault(require("./addLogEntry"));
 var addWorkoutExercise_1 = __importDefault(require("./addWorkoutExercise"));
 var createExercise_1 = __importDefault(require("./createExercise"));
 var createUser_1 = __importDefault(require("./createUser"));
@@ -12,6 +13,7 @@ var Mutation = {
     newWorkoutExercise: addWorkoutExercise_1.default,
     deleteWorkout: deleteWorkout_1.default,
     newExercise: createExercise_1.default,
+    newLogEntry: addLogEntry_1.default,
     newUser: createUser_1.default,
     newWorkout: createWorkout_1.default,
 };
